d767e18a9aed0cad13a8bb68609f73ed0f6c5ae3
pietrolupi <pietro.lupi92@gmail.com>
2023-09-14 14:24:25 +0200

    [edit]: js,html

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+
+let listaSpesa = ['vino', 'sigarette', 'vodka', 'birra', 'noodles'];
+const messaggio = document.getElementById('messaggio');
+
+const btnAzione = document.getElementById('btn-spesa');
+
+let counterSpesa = 0;
+let check = false;
+
+
+btnAzione.addEventListener('click', function(){
+
+  while(!check){
+   
+    if(counterSpesa === listaSpesa.length){
+      check = true;
+    }else{
+      messaggio.innerHTML += `<h1>${listaSpesa[counterSpesa]}</h1>`;
+      counterSpesa ++;
+    }
+   
+  }
+
+})
+
+
+
